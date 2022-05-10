2b2f0dffc844ec230eb5f7bbb4bf9101c86f2869
DelfinaBusigina <delfina.busigina@gmail.com>
2022-05-10 13:14:27 +0300

    Fourth upload

--- src/SnakeGame.java
+++ src/SnakeGame.java
@@ -1,6 +1,18 @@
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
 public class SnakeGame {
     public static void main(String[] args) throws Exception {
-        new GameFrame(); //shortcut to not write GameFrame gameFrame = new...
+        
+        //shortcut to not write GameFrame gameFrame = new...
+        new GameFrame();
+        
         
     }
 }
